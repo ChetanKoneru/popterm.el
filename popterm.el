d086833f7762d7f9a96954451328ee9a57378b93
Chetan Koneru <kchetan.hadoop@gmail.com>
2026-09-17 15:31:27 -0400

    Update popterm.el

--- popterm.el
+++ popterm.el
@@ -6,7 +6,7 @@
 ;; Version: 1.0.0
 ;; Package-Requires: ((emacs "29.1") (posframe "1.4.0"))
 ;; Keywords: terminals, convenience, tools
-;; URL: https://github.com/CsBigDataHub/popterm.el
+;; URL: https://github.com/ChetanKoneru/popterm.el
 
 ;; This file is not part of GNU Emacs.
 ;;
